5b1893293db079b4f8c283869eb2cc308902a1d9
sulei <779647751@qq.com>
2022-09-27 21:24:58 +0800

    initial

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Layout from '../components/Layout'
+
 
 Vue.use(VueRouter)
 
@@ -9,6 +11,18 @@ const routes = [
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    name: 'home',
+    path: '/home',
+    component: Layout,
+    children: [
+      {
+        path: '/home/demo-list',
+        name: 'home/demo-list',
+        component: () => import('../views/DemoList.vue'),
+      },
+    ]
   },
   {
     path: '/about',
